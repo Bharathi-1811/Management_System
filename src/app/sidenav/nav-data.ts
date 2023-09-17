51020919139f941678284bd017b294c63a107b4a
shanmugi-M <142895108+shanmugi-M@users.noreply.github.com>
2023-09-17 14:43:07 +0530

    Add files via upload
    Dashboard

---
+++ src/app/sidenav/nav-data.ts
@@ -0,0 +1,45 @@
+export const navbarData =[
+    {
+        routeLink:'dashboard',
+        icon:'fa fa-home',
+        label:'Dashboard'
+    },
+    {
+        routeLink:'products',
+        icon:'fal fa-box-open',
+        label:'products'
+
+    },
+    {
+        routeLink:'pages',
+       icon:'fal fa-file' ,
+        label:'Pages'
+    },
+
+    {
+        routeLink:'statistics',
+        icon:'fal fa-chart-bar',
+        label:'statistics'
+
+    },
+    {
+        routeLink:'coupens',
+        icon:'fal fa-tags',
+        label:'Coupens'
+
+    },
+    {
+        routeLink:'media',
+        icon:'fal fa-camera',
+        label:'Media'
+
+    },
+    {
+        routeLink:'settings',
+        icon:'fal fa-cog',
+        label:'settings'
+
+    },
+
+
+];
